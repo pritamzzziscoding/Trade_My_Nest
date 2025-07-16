ea77b44ccc07c6018721b4e0beae170cf742e6fa
Pritam Barman <pritambarmanofficial1608@gmail.com>
2025-07-16 18:42:26 +0530

    version-2.0 debug

--- frontend/src/libs/apis.jsx
+++ frontend/src/libs/apis.jsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    // baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
-    baseURL : "http://localhost:3000/api",
+    baseURL : import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    // baseURL : "http://localhost:3000/api",
     withCredentials : true
 })
 
